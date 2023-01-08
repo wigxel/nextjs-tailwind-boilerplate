f4e8b87152bfc3528777c30cef28ab19d49ab62f
Joseph Owonvwon <joseph.owonwo@gmail.com>
2023-01-08 03:53:57 +0100

    Upgraded to Next 13

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,15 @@ import "../styles/tailwind.css";
 import Theme from "../styles/Theme";
 // @ts-ignore
 import { ThemeProvider } from "@wigxel/react-components";
+import { bodyFont } from "../styles/font";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={Theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <main className={`${bodyFont.variable} font-body`}>
+      <ThemeProvider theme={Theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </main>
   );
 }
