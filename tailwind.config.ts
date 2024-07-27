905404711b931516ba788ab10f90f5d1a8ad610d
Joseph Owonvwon <joseph.owonwo@gmail.com>
2024-07-28 00:09:17 +0100

    chore: added and configured biome

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
-const { fontFamily } = require("tailwindcss/defaultTheme");
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 module.exports = {
- content: [
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -20,11 +20,11 @@ module.exports = {
         _4: "#FF374F",
       },
     },
-     backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    backgroundImage: {
+      "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      "gradient-conic":
+        "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    },
   },
   variants: {},
   plugins: [],
