326cf3dad3ad3869122aa03b3741303bf68e2696
Joseph Owonvwon <joseph.owonwo@gmail.com>
2025-04-30 01:12:13 +0100

    config react-query and consola logger

---
+++ components/providers/react-query.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export const queryClient = new QueryClient();
+
+export const QueryProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
