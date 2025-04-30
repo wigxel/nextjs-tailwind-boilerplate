33ccab8dbc2178f30f68eb97457ab2858424eefd
Joseph Owonvwon <joseph.owonwo@gmail.com>
2025-04-30 01:01:40 +0100

    chore: switch to yarn to pnpm, jest to vite and update packages

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import { FlatCompat } from "@eslint/eslintrc"
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+})
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+    ignorePatterns: [
+      "node_modules",
+      "public",
+      "widget/*",
+      "pages/*",
+      "utils/*",
+      "lib/web3-connect/*",
+      "tests/setup.ts",
+      "**/*.test.ts",
+      "**/*.test.tsx",
+    ],
+  }),
+]
+
+export default eslintConfig
---
+++ vitest.config.mjs
@@ -0,0 +1,39 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vitest/config"
+import react from "@vitejs/plugin-react-swc"
+import tsconfigPaths from "vite-tsconfig-paths"
+import { config } from "dotenv"
+
+// load .env.test
+config({
+  path: ".env.testing",
+})
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    css: true,
+    env: {
+      NEXT_PUBLIC_ALCHEMY_API_KEY: String(
+        process.env.NEXT_PUBLIC_ALCHEMY_API_KEY,
+      ),
+    },
+    setupFiles: "./tests/setup.ts",
+    reporters: ["verbose"],
+    environment: "jsdom",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      exclude: [
+        "node_modules/**",
+        "test/**",
+        "./.next",
+        "./components/icons/**",
+        "./components/ui/**",
+        "./components/wallet/avatars/**",
+        "./**/__tests__/**",
+      ],
+    },
+  },
+})
